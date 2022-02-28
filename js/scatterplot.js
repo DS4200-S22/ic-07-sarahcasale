013226d34b0616847292e0f797de433b980e2de2
sarahcasale <39677591+sarahcasale@users.noreply.github.com>
2022-02-28 12:59:00 -0500

    last push

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -7,6 +7,22 @@ Modified: 12/08/21
 */
 
 // Build your scatterplot in this file 
+const scatterwidth = 900; 
+const scatterheight = 450; 
+const scattermargin = {left:50, right:50, bottom:50, top:50}; 
+
+const mouseover = function (event, d) {
+  let point = d3.select(this); 
+  point.classed("myFirstPlot", false);
+  point.classed("highlighted", true);
+}
+
+const mouseout = function (event, d) {
+  let point = d3.select(this); 
+  point.classed("myFirstPlot", true);
+  point.classed("highlighted", false);
+
+}
 
 d3.select("svg")  //select the first svg tag in the DOM
   .append("circle") // append a circle to the svg
@@ -15,19 +31,12 @@ d3.select("svg")  //select the first svg tag in the DOM
     .attr("r", 30)  // set r 
     .attr("class", "firstCircle");
 
-const width = 900; 
-const height = 450; 
-const margin = {left:50, right:50, bottom:50, top:50}; 
-const yTooltipOffset = 15; 
-
-/*
-// First, let's add a new svg to build within. 
 let svg = d3
   .select("#csv-scatter")
   .append("svg")
-  .attr("width", width-margin.left-margin.right)
-  .attr("height", height - margin.top - margin.bottom)
-  .attr("viewBox", [0, 0, width, height]);
+    .attr("width", scatterwidth- scattermargin.left - scattermargin.right)
+    .attr("height", scatterheight - scattermargin.top - scattermargin.bottom)
+    .attr("viewBox", [0, 0, scatterwidth, scatterheight])
 
 d3.csv("data/scatter.csv").then((data) => { 
 
@@ -47,35 +56,49 @@ d3.csv("data/scatter.csv").then((data) => {
         .attr("cx", (d) => { return d.x; }) // use x for cx
         .attr("cy", (d) => { return d.y; }) // use y for cy
         .attr("r", 30)  // set r 
-});
-*/
-// To start, we will use hard coded data.
-const data1 = [100, 200, 300];
+        .on("mouseover", mouseover) // mouseover listener
+        .on("mouseout", mouseout);
 
 // First, let's add a new svg to build within. 
-let svg = d3.select("#csv-scatter")
-            .append("svg")
-              .attr("class", "holder");  
+// find max X
+let scattermaxX = d3.max(data, (d) => { return d.x; }); 
+console.log("Scatter Max X: " + scattermaxX); 
 
-// We will add a circle for each datum in data1 to the svg 
-// we added above.
-svg.selectAll(".scatter") // select all circle tags in svg
-                        //  Note: this is weird! There are
-                        //  no circles in our svg. This is
-                        //  an empty selection. 
-    .data(data1)  // empty select from above gets passed here
-                  // we also pass in out data
-                  // data() joins the two
-    .enter()  // joined empty selection and data get passed here
-              // enter() creates a placeholder DOM element 
-              // for all data that is not yet associated with 
-              // a the DOM element  
-    .append("circle");  // placeholders bound to data passed here
-                        // appends a circle to svg for each 
-                        // placeholder 
+// find max Y 
+let scattermaxY = d3.max(data, (d) => { return d.y; }); 
+console.log("Scatter Max Y: " + scattermaxY); 
 
+// Now that we have our maxes we define scale functions that
+// map our data values (domain for the scale function) to our
+// pixel values (range for the scale function)
 
+let scatterxScale = d3.scaleLinear() // linear scale because we have 
+                              // linear data 
+                .domain([0, scattermaxX])  // inputs for the function
+                .range([scattermargin.left, scatterwidth - scattermargin.right]); 
+                // ^ outputs for the function 
 
+let scatteryScale = d3.scaleLinear()
+            .domain([0, scattermaxY])
+            .range([scatterheight - scattermargin.bottom, scattermargin.top]); 
+
+// Add x axis to svg6  
+svg.append("g") // g is a "placeholder" svg
+      .attr("transform", `translate(0,${scatterheight - scattermargin.bottom})`) 
+      // ^ moves axis to bottom of svg 
+      .call(d3.axisBottom(scatterxScale)) // built in function for bottom
+                                  // axis given a scale function 
+        .attr("font-size", '20px'); // set font size
+
+// Add y axis to svg6 
+svg.append("g") // g is a "placeholder" svg
+     .attr("transform", `translate(${scattermargin.left}, 0)`) 
+     // ^ move axis inside of left margin
+     .call(d3.axisLeft(scatteryScale)) // built in function for left
+                                // axis given a scale function 
+      .attr("font-size", '20px'); // set font size
+
+});
 
 
 
